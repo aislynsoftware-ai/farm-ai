e6e7c0149e21f2cc1588a041e689e900048e6bce
Ajay Duraisamy <ajayduraisamy@gmail.com>
2026-06-15 11:50:40 +0530

    vite conig

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    minify: 'esbuild',
-    cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          motion: ['framer-motion'],
-        },
-      },
-    },
-  },
 })
